b5cc2f1ba34a4884c11c1bd9745f9037e7c9d0e3
bahman <bahman026@gmail.com>
2022-11-11 11:52:38 +0330

    update route to any

--- routes/api.php
+++ routes/api.php
@@ -12,5 +12,5 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/', 'BotController@index');
+Route::any('/', 'BotController@index');
 
